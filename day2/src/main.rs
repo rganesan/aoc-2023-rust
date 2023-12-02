e2b811c2e3f52b1e31703d53c9f530495ea9252e
Ganesan Rajagopal <rganesan@gmail.com>
2023-12-02 16:04:56 +0530

    Remove unwraps
    Copying best practices :-)

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Error, Result};
 use std::cmp::max;
 use std::env;
 use std::fs::File;
@@ -7,15 +7,20 @@ use std::time::Instant;
 
 type CubeSet = [u32; 3];
 
-fn parse_cubeset(cubeset_str: &str) -> CubeSet {
+fn parse_cubeset(cubeset_str: &str) -> Result<CubeSet> {
     let mut cubeset = [0, 0, 0];
     const COLORS: [&str; 3] = ["red", "green", "blue"];
     for num_and_cube in cubeset_str.split(',') {
-        let (num, color) = num_and_cube[1..].split_once(' ').unwrap();
-        let pos = COLORS.iter().position(|c| c == &color).unwrap();
-        cubeset[pos] = num.parse::<u32>().unwrap();
+        let (num, color) = num_and_cube[1..]
+            .split_once(' ')
+            .ok_or_else(|| Error::msg("invalid cube configuration"))?;
+        let pos = COLORS
+            .iter()
+            .position(|c| c == &color)
+            .ok_or_else(|| Error::msg("invalid color"))?;
+        cubeset[pos] = num.parse::<u32>()?;
     }
-    cubeset
+    Ok(cubeset)
 }
 
 fn part1(filename: &str) -> Result<u32> {
@@ -26,9 +31,11 @@ fn part1(filename: &str) -> Result<u32> {
     'game: for line in reader.lines() {
         let line = line?;
         // Format: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-        let (hdr, game_plays) = line.split_once(':').unwrap();
+        let (hdr, game_plays) = line
+            .split_once(':')
+            .ok_or_else(|| Error::msg("invalid game format"))?;
         for game_play in game_plays.split(';') {
-            let cubeset = parse_cubeset(game_play);
+            let cubeset = parse_cubeset(game_play)?;
             for (bag, cube) in CUBE_BAG.iter().zip(cubeset.iter()) {
                 if bag < cube {
                     // this game is impossible
@@ -49,10 +56,12 @@ fn part2(filename: &str) -> Result<u32> {
     for line in reader.lines() {
         let line = line?;
         // Format: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-        let (_hdr, game_plays) = line.split_once(':').unwrap();
+        let (_hdr, game_plays) = line
+            .split_once(':')
+            .ok_or_else(|| Error::msg("invalid game format"))?;
         let mut min_cubeset = [0, 0, 0];
         for game_play in game_plays.split(';') {
-            let cubeset = parse_cubeset(game_play);
+            let cubeset = parse_cubeset(game_play)?;
             for (min_color, color) in min_cubeset.iter_mut().zip(cubeset.iter()) {
                 *min_color = max(*min_color, *color);
             }
@@ -71,7 +80,7 @@ fn main() -> Result<()> {
     let sum1 = part1(&filename).expect("failed");
     let duration1 = start1.elapsed();
     println!("part1: {sum1}, time: {duration1:?}");
-    
+
     let start2 = Instant::now();
     let sum2 = part2(&filename).expect("failed");
     let duration2 = start2.elapsed();
