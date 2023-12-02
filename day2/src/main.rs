267314c1b536404a823bf9109f1ae6d4b0a2487b
Ganesan Rajagopal <rganesan@gmail.com>
2023-12-02 15:31:02 +0530

    Use vector to keep track of cubes
    This simplifies the logic using some functional programming

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -5,54 +5,40 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::Instant;
 
-const RED: u32 = 12;
-const GREEN: u32 = 13;
-const BLUE: u32 = 14;
+type CubeSet = [u32; 3];
 
-#[derive(Debug)]
-struct RGB {
-    r: u32,
-    g: u32,
-    b: u32,
-}
-
-fn parse_rgb(play: &str) -> RGB {
-    let mut rgb = RGB { r: 0, g: 0, b: 0 };
-    for cube in play.split(',') {
-        let mut cube_iter = cube[1..].split(' ');
-        let num = cube_iter.next().unwrap().parse::<u32>().unwrap();
-        let color = cube_iter.next().unwrap();
-        match color {
-            "red" => rgb.r = num,
-            "green" => rgb.g = num,
-            "blue" => rgb.b = num,
-            _ => panic!("Invalid color {color}"),
-        };
+fn parse_cubeset(cubeset_str: &str) -> CubeSet {
+    let mut cubeset = [0, 0, 0];
+    const COLORS: [&str; 3] = ["red", "green", "blue"];
+    for num_and_cube in cubeset_str.split(',') {
+        let (num, color) = num_and_cube[1..].split_once(' ').unwrap();
+        let pos = COLORS.iter().position(|c| c == &color).unwrap();
+        cubeset[pos] = num.parse::<u32>().unwrap();
     }
-    rgb
+    cubeset
 }
 
 fn part1(filename: &str) -> Result<u32> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
     let mut sum = 0;
+    const CUBE_BAG: [u32; 3] = [12, 13, 14]; // Number of red, green, blue in bag
     for line in reader.lines() {
         let line = line?;
-        // println!("{line}");
         // Format: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-        let mut line_iter = line.split(':');
-        let game_id = line_iter.next().unwrap()[5..].parse::<u32>()?;
+        let (hdr, games) = line.split_once(':').unwrap();
+        let game_id = hdr[5..].parse::<u32>()?;
         let mut game_possible = true;
-        for game in line_iter.next().unwrap().split(';') {
-            let rgb = parse_rgb(game);
-            // println!("{rgb:?}");
-            if rgb.r > RED || rgb.g > GREEN || rgb.b > BLUE {
-                game_possible = false;
-                break;
+        'game: for game in games.split(';') {
+            let cubeset = parse_cubeset(game);
+            for (bag, cube) in CUBE_BAG.iter().zip(cubeset.iter()) {
+                if bag < cube {
+                    game_possible = false;
+                    break 'game;
+                }
             }
         }
         if game_possible {
-            // println!("{line} possible");
             sum += game_id;
         }
     }
@@ -65,20 +51,16 @@ fn part2(filename: &str) -> Result<u32> {
     let mut sum = 0;
     for line in reader.lines() {
         let line = line?;
-        // println!("{line}");
         // Format: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-        let mut line_iter = line.split(':');
-        let _game_id = line_iter.next().unwrap()[5..].parse::<u32>()?;
-        let mut min_rgb = RGB { r: 0, g: 0, b: 0 };
-        for game in line_iter.next().unwrap().split(';') {
-            let rgb = parse_rgb(game);
-            // println!("{rgb:?}");
-            min_rgb.r = max(rgb.r, min_rgb.r);
-            min_rgb.g = max(rgb.g, min_rgb.g);
-            min_rgb.b = max(rgb.b, min_rgb.b);
+        let (_hdr, games) = line.split_once(':').unwrap();
+        let mut min_cubeset = [0, 0, 0];
+        for game in games.split(';') {
+            let cubeset = parse_cubeset(game);
+            for (min_color, color) in min_cubeset.iter_mut().zip(cubeset.iter()) {
+                *min_color = max(*min_color, *color);
+            }
         }
-        let power = min_rgb.r * min_rgb.g * min_rgb.b;
-        // println!("min_rgb: {min_rgb:?}");
+        let power: u32 = min_cubeset.iter().product();
         sum += power;
     }
     Ok(sum)
@@ -87,11 +69,12 @@ fn part2(filename: &str) -> Result<u32> {
 fn main() -> Result<()> {
     let filename = env::args()
         .nth(1)
-        .unwrap_or_else(|| "inputs/test1.txt".to_string());
+        .unwrap_or_else(|| "inputs/input.txt".to_string());
     let start1 = Instant::now();
     let sum1 = part1(&filename).expect("failed");
     let duration1 = start1.elapsed();
     println!("part1: {sum1}, time: {duration1:?}");
+    
     let start2 = Instant::now();
     let sum2 = part2(&filename).expect("failed");
     let duration2 = start2.elapsed();
