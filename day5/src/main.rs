c0782c768366281dba76825ffe7848ecec18884d
Ganesan Rajagopal <rganesan@gmail.com>
2023-12-05 21:06:50 +0530

    Comment out printfs

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -73,7 +73,7 @@ fn map_seed_to_location(seed: usize, maps: &Vec<Maps>) -> usize {
         }
         // println!("{mapping}");
     }
-    println!("seed: {seed}, location: {mapping}");
+    // println!("seed: {seed}, location: {mapping}");
     mapping
 }
 
@@ -97,7 +97,7 @@ fn seed_range_to_min_location(seed: usize, len: usize, maps: &Vec<Maps>) -> usiz
             for m in map {
                 if mapping >= m.src && mapping < m.src + m.len {
                     let remaining_span = m.len - (mapping - m.src);
-                    println!("seed {s} remaining_span {remaining_span}");
+                    // println!("seed {s} remaining_span {remaining_span}");
                     if m.len < min_span {
                         min_span = remaining_span;
                     }
@@ -105,14 +105,14 @@ fn seed_range_to_min_location(seed: usize, len: usize, maps: &Vec<Maps>) -> usiz
                     break;
                 }
             }
-            println!("seed {s}, mapping {mapping} min_span {min_span}");
+            // println!("seed {s}, mapping {mapping} min_span {min_span}");
         }
         s += min_span;
         if mapping < min_mapping {
             min_mapping = mapping;
         }
     }
-    println!("seed: {seed}, location: {min_mapping}");
+    // println!("seed: {seed}, location: {min_mapping}");
     min_mapping
 }
 
@@ -136,7 +136,7 @@ fn main() -> Result<()> {
     let (seeds, maps) = parse_almanac(&filename)?;
 
     let start1 = Instant::now();
-    let sum1 = 0; // part1(&seeds, &maps);
+    let sum1 = part1(&seeds, &maps);
     let duration1 = start1.elapsed();
     println!("part1: {sum1}, time: {duration1:?}");
 
